e1f0d54099c52153f24c98e77ae7189e60b299fa
Maria Camila Restrepo <mariacamilarestrepo@gmail.com>
2021-10-27 16:02:53 -0500

    Conexión back y front completa y algo de autenticación

--- src/App.jsx
+++ src/App.jsx
@@ -8,48 +8,57 @@ import Inicio from './pages/Inicio';
 import PublicLayout from './layouts/PublicLayout';
 import PrivateLayout from 'layouts/PrivateLayout';
 import LoginLayout from 'layouts/LoginLayout';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function App() {
 
   return (  
-    <Router>
-      <Switch>
-        <Route path={['/login']}>
-          <LoginLayout>
-            <Switch>
-              <Route path='/login'>
-                <Login/>
-              </Route>
-            </Switch>
-          </LoginLayout>
-        </Route>
-        <Route path={['/gestionProductos','/gestionVentas',
-         '/usuarios', ]}>
-          <PrivateLayout>
-            <Switch>
-              <Route path='/gestionProductos'>
-                <RegistroProductos/>
-              </Route>
-              <Route path='/gestionVentas'>
-                <GestionVentas/>
-              </Route>
-              <Route path='/usuarios'>
-                <Usuarios/>
-              </Route>
-            </Switch>
-          </PrivateLayout>
-        </Route>
-        <Route path={['/']}>
-          <PublicLayout>
-            <Switch>
-              <Route path='/'>
-                <Inicio/>
-              </Route>
-            </Switch>
-          </PublicLayout>
-        </Route>
-      </Switch>
-    </Router>
+    <Auth0Provider
+    domain="misiontic-komuya.us.auth0.com"
+    clientId="elQinPRmYGjXj6UK2K3tDb80i83wGAcH"
+    redirectUri={window.location.origin}
+    >
+      <div>
+        <Router>
+          <Switch>
+            <Route path={['/login']}>
+              <LoginLayout>
+                <Switch>
+                  <Route path='/login'>
+                    <Login/>
+                  </Route>
+                </Switch>
+              </LoginLayout>
+            </Route>
+            <Route path={['/gestionProductos','/gestionVentas',
+            '/usuarios', ]}>
+              <PrivateLayout>
+                <Switch>
+                  <Route path='/gestionProductos'>
+                    <RegistroProductos/>
+                  </Route>
+                  <Route path='/gestionVentas'>
+                    <GestionVentas/>
+                  </Route>
+                  <Route path='/usuarios'>
+                    <Usuarios/>
+                  </Route>
+                </Switch>
+              </PrivateLayout>
+            </Route>
+            <Route path={['/']}>
+              <PublicLayout>
+                <Switch>
+                  <Route path='/'>
+                    <Inicio/>
+                  </Route>
+                </Switch>
+              </PublicLayout>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </Auth0Provider>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,17 @@
 import logo from 'media/logo.png'
 import React from 'react'
 import 'styles/estilos.css'
-import { Link } from 'react-router-dom'
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
+    const { loginWithRedirect } = useAuth0();
     return (
         <nav className='blue-dark'>
             <ul className='nav flex w-full justify-between my-3'>
                 <li><img className='w-40' src={logo} alt="Komuya.png" /></li>
                 <li className='titulo'>KOMUYA</li>
                 <li>
-                    <Link to='/login'>
-                        <button className='button align-bottom'>Iniciar Sesión</button>
-                    </Link>
+                    <button onClick={() => loginWithRedirect()} className='button align-bottom'>Iniciar Sesión</button>
                 </li>
             </ul>
         </nav>
